971850496fd134074abbe61b83b7e8e5f4e7d05e
RapidRapidRabbit <awestruck31@mail.ru>
2022-11-05 05:40:19 +0300

    working with header

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage/HomePage";
 
 const Router = () => {
     return <BrowserRouter>
 <Routes>
-    <Route path="/" element = {<div>Hello</div>}/>
+    <Route path="/" element = {<HomePage/>}/>
 </Routes>
     </BrowserRouter>
 }
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import Header from "../../components/Header/Header";
+import "./HomePage.css";
+
+export default function HomePage() {
+  return (
+    <Fragment>
+      <Header />
+      <div>This is Home PAge</div>
+    </Fragment>
+  );
+};
