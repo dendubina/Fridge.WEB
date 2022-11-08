1ae392457a578ba00da38fff4b925b3e64770e12
RapidRapidRabbit <awestruck31@mail.ru>
2022-11-08 14:05:01 +0300

    Update FridgesList.js

--- src/pages/Fridges/FridgesList/FridgesList.js
+++ src/pages/Fridges/FridgesList/FridgesList.js
@@ -1,7 +1,6 @@
 import "./FridgesList.css";
 import Header from "../../../components/Header/Header";
 import FridgesTable from "../../../components/FridgesTable/FridgesTable";
-import { Link } from "react-router-dom";
 
 export default function FridgesList() {
   return (
